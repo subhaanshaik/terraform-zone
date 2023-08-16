cdbedb88f6592a8ccf189fa2af72bba36d516133
subhaan <subhaan.subbu@gmail.com>
2023-08-16 16:59:00 +0530

    added subnets

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ resource "aws_vpc" "vpc1" {
 
   }
 }
+
+resource "aws_subnet" "subnets" {
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = var.subnet_cidr_ranges[count.index]
+  tags = {
+    name = var.subnet_names[count.index]
+  }
+  depends_on = [
+    aws_vpc.vpc1
+  ]
+}
