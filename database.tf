58ed915ea24a0da7396335ebeced3e3ded69c0b7
subhaan <subhaan.subbu@gmail.com>
2023-08-29 16:48:28 +0530

    Added rds instance

--- database.tf
+++ database.tf
@@ -28,6 +28,24 @@ resource "aws_db_subnet_group" "ntier" {
 }
 
 
+resource "aws_db_instance" "default" {
+  allocated_storage      = 20
+  db_name                = "emp"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  username               = "root"
+  password               = "rootroot"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.ntier.name
+  identifier             = "qtntierdbfromtf"
+  vpc_security_group_ids = [aws_security_group.dbsg.id]
+
+  depends_on = [
+    aws_db_subnet_group.ntier,
+    aws_subnet.subnets
+  ]
+}
 
 
 
